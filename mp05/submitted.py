f5127628793691c6f9a43eb8670ce99b8bdca21b
Steven Y M Chang <stevenymchang@Stevens-MacBook-M3Pro.local>
2024-02-15 15:24:17 -0600

    adding mp5 release files

---
+++ mp05/submitted.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+
+
+def create_sequential_layers():
+    """
+    Task: Create neural net layers using nn.Sequential.
+
+    Requirements: Return an nn.Sequential object, which contains:
+        1. a linear layer (fully connected) with 2 input features and 3 output features,
+        2. a sigmoid activation layer,
+        3. a linear layer with 3 input features and 5 output features.
+    """
+    raise NotImplementedError("You need to write this part!")
+
+
+def create_loss_function():
+    """
+    Task: Create a loss function using nn module.
+
+    Requirements: Return a loss function from the nn module that is suitable for
+    multi-class classification.
+    """
+    raise NotImplementedError("You need to write this part!")
+
+
+class NeuralNet(torch.nn.Module):
+    def __init__(self):
+        """
+        Initialize your neural network here.
+        """
+        super().__init__()
+        ################# Your Code Starts Here #################
+
+        raise NotImplementedError("You need to write this part!")
+        ################## Your Code Ends here ##################
+
+    def forward(self, x):
+        """
+        Perform a forward pass through your neural net.
+
+        Parameters:
+            x:      an (N, input_size) tensor, where N is arbitrary.
+
+        Outputs:
+            y:      an (N, output_size) tensor of output from the network
+        """
+        ################# Your Code Starts Here #################
+
+        raise NotImplementedError("You need to write this part!")
+        return y
+        ################## Your Code Ends here ##################
+
+
+def train(train_dataloader, epochs):
+    """
+    The autograder will call this function and compute the accuracy of the returned model.
+
+    Parameters:
+        train_dataloader:   a dataloader for the training set and labels
+        test_dataloader:    a dataloader for the testing set and labels
+        epochs:             the number of times to iterate over the training set
+
+    Outputs:
+        model:              trained model
+    """
+
+    ################# Your Code Starts Here #################
+    """
+    Implement backward propagation and gradient descent here.
+    """
+    # Create an instance of NeuralNet, a loss function, and an optimizer
+    model = NeuralNet()
+    loss_fn = None
+    optimizer = None
+
+    raise NotImplementedError("You need to write this part!")
+    ################## Your Code Ends here ##################
+
+    return model
