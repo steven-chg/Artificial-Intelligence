4938b6ff38a00bcd3dc0e52931690744d2dce19a
Steven Y M Chang <stevenymchang@wirelessprv-10-192-170-139.near.illinois.edu>
2024-03-25 18:40:32 -0500

    completed minimax

--- mp07/submitted.py
+++ mp07/submitted.py
@@ -40,6 +40,11 @@ def random(board, side, flags, chooser):
     else:
         return ([], {}, evaluate(board))
 
+'''
+Note
+- move[0] = from, move[1] = to
+'''
+    
 ###########################################################################################
 # Stuff you need to write:
 # Move-generating functions using minimax, alphabeta, and stochastic search.
@@ -56,8 +61,65 @@ def minimax(board, side, flags, depth):
       flags (list of flags): list of flags, used by generateMoves and makeMove
       depth (int >=0): depth of the search (number of moves)
     '''
-    raise NotImplementedError("you need to write this!")
+    # raise NotImplementedError("you need to write this!")
 
+    '''
+    Possible API Calls/Useful Definitions
+    - side == False is player 0 should play next
+    - move == [fro, to, promote], fro == [from_x, from_y], to == [to_x, to_y], promote = None or "q" (promote to queen)
+    - piece == [x, y, type]
+    - chess.lib.heuristics.evaluate(board) returns hueristic value of the board for the white player (Max)
+    - encode([from_x, from_y], [to_x, to_y], promote)
+    - chess.lib.convertMoves, generates starting board
+    - submitted.generateMoves(board, side, flags), generates all moves legal on the current board
+    - chess.lib.makeMove(side, board, [from_x, from_y], [to_x, to_y], flags, promote), returns newside, newboard, newflags
+
+    Other Notes
+    - player 0 white (goes first), player 1 black 
+    - side == False if player 0 plays next; side == True if player 1 plays next
+    '''
+
+    # base case, when depth is 0, evaluate the current board state, (no more moves to take, so return empty moveList and moveTree)
+    if depth == 0:
+        return [], {}, chess.lib.heuristics.evaluate(board)
+    
+    # initialize empty movelist list and empty moveTree dictionary
+    moveList = []
+    moveTree = {}
+
+    # Max/White player next; choose a path through this tree that maximizes the heuristic value of the final board
+    if side == False:
+        # initialize maxValue to negative infinity
+        maxValue = float('-inf')
+        # loop through all possible moves from current board condition
+        for maxMove in generateMoves(board, side, flags):
+            # make the move and recursively call minimax to explore all possible future moves/states
+            newMaxSide, newMaxBoard, newMaxFlags = chess.lib.makeMove(side, board, maxMove[0], maxMove[1], flags, maxMove[2])
+            rMaxList, rMaxTree, rMaxValue = minimax(newMaxBoard, newMaxSide, newMaxFlags, depth-1)
+            # update moveTree with subtree returned from minimax call; key will be the move that generated the subtree
+            moveTree[encode(maxMove[0], maxMove[1], maxMove[2])] = rMaxTree
+            # update maxValue and the optimal path if rMaxValue from recursive call greater than current maxValue
+            if rMaxValue > maxValue:
+                maxValue = rMaxValue
+                moveList = [maxMove] + rMaxList
+        return moveList, moveTree, maxValue
+    # Min/Black player next; choose a path through this tree that minimizes the heuristic value of the final board
+    elif side == True:
+        # initialize minValue to positive infinity
+        minValue = float('inf')
+        # loop through all possible moves from current board condition
+        for minMove in generateMoves(board, side, flags):
+            # make the move and recursively call minimax to explore all possible future moves/states
+            newMinSide, newMinBoard, newMinFlags = chess.lib.makeMove(side, board, minMove[0], minMove[1], flags, minMove[2])
+            rMinList, rMinTree, rMinValue = minimax(newMinBoard, newMinSide, newMinFlags, depth-1)
+            # update moveTree with subtree returned from minimax call; key will be the move that generated the subtree
+            moveTree[encode(minMove[0], minMove[1], minMove[2])] = rMinTree
+            # update minValue and the optimal path if rMinValue from recursive call is less than current minValue
+            if rMinValue < minValue:
+                minValue = rMinValue
+                moveList = [minMove] + rMinList
+        return moveList, moveTree, minValue
+  
 def alphabeta(board, side, flags, depth, alpha=-math.inf, beta=math.inf):
     '''
     Return minimax-optimal move sequence, and a tree that exhibits alphabeta pruning.
@@ -74,19 +136,20 @@ def alphabeta(board, side, flags, depth, alpha=-math.inf, beta=math.inf):
     raise NotImplementedError("you need to write this!")
     
 
-def stochastic(board, side, flags, depth, breadth, chooser):
-    '''
-    Choose the best move based on breadth randomly chosen paths per move, of length depth-1.
-    Return: (moveList, moveTree, value)
-      moveLists (list): any sequence of moves, of length depth, starting with the best move
-      moveTree (dict: encode(*move)->dict): a tree of moves that were evaluated in the search process
-      value (float): average board value of the paths for the best-scoring move
-    Input:
-      board (2-tuple of lists): current board layout, used by generateMoves and makeMove
-      side (boolean): True if player1 (Min) plays next, otherwise False
-      flags (list of flags): list of flags, used by generateMoves and makeMove
-      depth (int >=0): depth of the search (number of moves)
-      breadth: number of different paths 
-      chooser: a function similar to random.choice, but during autograding, might not be random.
-    '''
-    raise NotImplementedError("you need to write this!")
+# FROM PAST SEMESTERS
+# def stochastic(board, side, flags, depth, breadth, chooser):
+#     '''
+#     Choose the best move based on breadth randomly chosen paths per move, of length depth-1.
+#     Return: (moveList, moveTree, value)
+#       moveLists (list): any sequence of moves, of length depth, starting with the best move
+#       moveTree (dict: encode(*move)->dict): a tree of moves that were evaluated in the search process
+#       value (float): average board value of the paths for the best-scoring move
+#     Input:
+#       board (2-tuple of lists): current board layout, used by generateMoves and makeMove
+#       side (boolean): True if player1 (Min) plays next, otherwise False
+#       flags (list of flags): list of flags, used by generateMoves and makeMove
+#       depth (int >=0): depth of the search (number of moves)
+#       breadth: number of different paths 
+#       chooser: a function similar to random.choice, but during autograding, might not be random.
+#     '''
+#     raise NotImplementedError("you need to write this!")
